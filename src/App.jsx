da3c74fe992a04796d7c39e22f34e8e4ea061704
Morgane Bourdillon <mlacroix8@gmail.com>
2026-05-15 09:25:45 +0200

    set up kaza

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import RouterComponent from "./router/Router"
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <RouterComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/router/Router.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Error from '../pages/Error';
+
+function RouterComponent() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default RouterComponent;
