5f53ce2dc33b437a3a59ae2f777d8cca57d2d892
Morgane Bourdillon <mlacroix8@gmail.com>
2026-05-15 10:23:40 +0200

    footer set up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,16 @@
+import { NavLink } from 'react-router-dom'
+import logo from '../assets/logo-white.png'
+import styles from '../styles/Footer.module.scss'
+
+
+
 function Footer() {
-  return <footer></footer>
+  return <footer className={styles.footer}>
+      <NavLink to="/">
+        <img src={logo} alt="Kaza" className={styles.logo} />
+      </NavLink>
+      <p className={styles.copyright}>© 2020 Kasa. All rights reserved</p>
+  </footer>
 }
 
 export default Footer
